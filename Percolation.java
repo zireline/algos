f58043d733c97966e1c9fe9b51a7dc48e7fdef5a
jeromecabugwason <jeromecabugwason2@protonmail.com>
2024-01-23 10:18:30 +0800

    Add Percolation class and remove GridSites class

--- Percolation.java
+++ Percolation.java
@@ -12,7 +12,7 @@
 // @Command(name = "hello", mixinStandardHelpOptions = true, version = "hello 0.1", description = "hello made with jbang")
 // class Main implements Callable<Integer> {
 
-class Main {
+class Percolation {
 
   // @Parameters(index = "0", description = "The greeting to print", defaultValue
   // = "World!")
@@ -78,6 +78,46 @@ class GridSites {
       for (int j = 0; j < size; j++) {
         System.out.print(grid[i][j] + " ");
       }
+    }
+  }
+}
+
+class WeightedQuickUnion {
+  private int[] id;
+  private int[] sz;
+
+  public WeightedQuickUnion(int N) {
+    id = new int[N];
+    sz = new int[N];
+    for (int i = 0; i < N; i++) {
+      id[i] = i;
+      sz[i] = 1;
+    }
+  }
+
+  private int root(int i) {
+    while (i != id[i]) {
+      id[i] = id[id[i]]; // path compression
+      i = id[i];
+    }
+    return i;
+  }
+
+  public boolean connected(int p, int q) {
+    return root(p) == root(q);
+  }
+
+  public void union(int p, int q) {
+    int i = root(p);
+    int j = root(q);
+    if (i == j)
+      return;
+    if (sz[i] < sz[j]) {
+      id[i] = j;
+      sz[j] += sz[i];
+    } else {
+      id[j] = i;
+      sz[i] += sz[j];
     }
   }
 }
